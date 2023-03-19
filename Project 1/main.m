e1f4633cd94b31d0a1831e4ffc5c59f42855f0b0
eliasHw <76268791+eliasHw@users.noreply.github.com>
2023-03-19 17:16:42 -0400

    Added bitstream

--- Project 1/main.m
+++ Project 1/main.m
@@ -1,3 +1,22 @@
+% Data string, 63 characters
+data = 'WirelessCommunicationSystemsandSecurityDominicAssiaEliasKhamisy';
+
+% Convert to 8-bit ascii
+bitstream = dec2bin(double(data), 8);
+
+% Create 1ms source bitstream, 64QAM
+bitstream_duration = 1e-3;
+bits_per_symbol = 6;
+
+fs = 30.72e6;   % Sample frequency
+f = 15e3;       % 15KHz, subcarrier bandwidth
+T = 1/f;        % 60us
+
+% Repeat bitstream until its duration is 1ms
+for index=bitstream_duration/T
+    bitstream = cat(1, bitstream, bitstream);
+end
+
 % Init some constants
 a = 1/sqrt(2);
 b = 1/sqrt(10);
@@ -11,8 +30,10 @@ g = 7/sqrt(42);
 bpsk = [a+a, -a-a];
 qpsk = [a+a, a-a, -a+a, -a-a];
 qam_16 = [...
-    b+b, b+c, c+b, c+c, b-b, b-c, c-b, c-c,...
-    -b+b, -b+c, -c+b, -c+c, -b-b, -b-c, -c-b, -c-c];
+    b+b, b+c, c+b, c+c,...
+    b-b, b-c, c-b, c-c,...
+    -b+b, -b+c, -c+b, -c+c,...
+    -b-b, -b-c, -c-b, -c-c];
 qam_64 = [...
     e+e, e+d, d+e, d+d, e+f, e+g, d+f, d+g,...
     f+e, f+d, g+e, g+d, f+f, f+g, g+f, g+g,...
@@ -22,3 +43,7 @@ qam_64 = [...
     -f+e, -f+d, -g+e, -g+d, -f+f, -f+g, -g+f, -g+g,...
     -e-e, -e-d, -d-e, -d-d,-e-f, -e-g, -d-f, -d-g,...
     -f-e, -f-d, -g-e, -g-d,-f-f, -f-g, -g-f, -g-g];
+
+
+
+
