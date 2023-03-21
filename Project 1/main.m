d944b861e41a47f919b657726d039f785a749ab6
Dominic Assia <50758804+dominicassia@users.noreply.github.com>
2023-03-21 09:14:00 -0400

    src str, mod mapping, and cleaned symbol writing

--- Project 1/main.m
+++ Project 1/main.m
@@ -3,15 +3,17 @@ clc;
 clear all;
 close all;
 %%    
-% the string is: "WirelessCommunicationSystemsandSecurityEliasKhamisy"
-% Covert to binary we get
-src_str_1 = '010101110110100101110010011001010110110001100101011100110111001101000011011011110110110101101101011101010110111001101001011000110110000101110100011010010110111101101110010100110111100101110011011101000110010101101101011100110110000101101110011001000101001101100101011000110111010101110010011010010111010001111001010001010110110001101001011000010111001101001011011010000110000101101101011010010111001101111001';
+
+%src_str_1 = '010101110110100101110010011001010110110001100101011100110111001101000011011011110110110101101101011101010110111001101001011000110110000101110100011010010110111101101110010100110111100101110011011101000110010101101101011100110110000101101110011001000101001101100101011000110111010101110010011010010111010001111001010001010110110001101001011000010111001101001011011010000110000101101101011010010111001101111001';
 
 str = 'WirelessCommunicationSystemsandSecurityEliasKhamisy';
 % Convert to 8-bit ascii
 str_ascii = dec2bin(double(str), 8);
-src_str = reshape(str_ascii, 1, []);
-
+% Convert to single horz. string
+src_str = '';
+for str_idx=1:length(str_ascii)
+    src_str = strcat(src_str, str_ascii(str_idx, :));
+end
 % 1 sub-frame worth of 64QAM symbols has the src_str repeated 422 time
 input_str = repmat(src_str,1,422); % (1) Input stream
 % split input_str into equal size strings of size 6 for 64QAM
@@ -23,11 +25,27 @@ qam64_vec = qam64_vec(1:2048*14); % trim vector to fit 1 subframe
 qam64_dec_vec = bin2dec(qam64_vec); % convert from binary string to decimal
 qam64_dec_vec = transpose(qam64_dec_vec);
 
+% Constellations
+% Init some constants
+d = 1/sqrt(42);
+e = 3/sqrt(42);
+f = 5/sqrt(42);
+g = 7/sqrt(42);
+qam_64_key = 0:1:63; % vector with values 0 to 63
+qam_64 = [...
+    e+e, e+d, d+e, d+d, e+f, e+g, d+f, d+g,...
+    f+e, f+d, g+e, g+d, f+f, f+g, g+f, g+g,...
+    e-e, e-d, d-e, d-d, e-f, e-g, d-f, d-g,...
+    f-e, f-d, g-e, g-d, f-f, f-g, g-f, g-g,...
+    -e+e, -e+d -d+e, -d+d, -e+f,-e+g, -d+f, -d+g,...
+    -f+e, -f+d, -g+e, -g+d, -f+f, -f+g, -g+f, -g+g,...
+    -e-e, -e-d, -d-e, -d-d,-e-f, -e-g, -d-f, -d-g,...
+    -f-e, -f-d, -g-e, -g-d,-f-f, -f-g, -g-f, -g-g];
+qam_64_mod = dictionary(qam_64_key ,qam_64); % map key to value
+qam64_sym_vec = qam_64_mod(qam64_dec_vec); % modulate
 
-% FIXME - map with out constellation
-qam64_sym_vec = qammod(qam64_dec_vec,64);
+%qam64_sym_vec = qammod(qam64_dec_vec,64);  %% DELETE - Reference only
 
-qam64_sym_vec = qam64_sym_vec / sqrt(42); % based on table 7.1.4-1. (2) bits to symbols 64QAM
 % Serial to parallel conversion to create OFDM vectors of 2048 symbols each
 qam64_sym_sf = reshape(qam64_sym_vec, 2048, []); % organize as 2048x14 subframe. (3) Serial to Parallel
 % Compute IFFT on the subframe
@@ -48,8 +66,15 @@ col_last160 = col(2048-160+1:2048);
 qam64_tx_sym_1 = [col_last160, col];
 save ofdm_tx_sym.mat qam64_tx_sym_1;
 
-% add cyclic check to the remaining 6 symbols
-for idx_1=2:7
+% 2nd
+col = qam64_sf_ifft(:,2);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_2 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_2;
+
+% add cyclic check to the remaining 5 symbols
+for idx_1=3:7
     col = qam64_sf_ifft(:,idx_1);
     col = transpose(col);
     col_last144 = col(2048-144+1:2048);
--- Project 1/notes.m
+++ Project 1/notes.m
@@ -1,26 +1,5 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Part 1 Notes - 
-% Data string, 63 characters
-data = 'WirelessCommunicationSystemsandSecurityDominicAssiaEliasKhamisy';
-
-% Convert to 8-bit ascii
-bitstream = dec2bin(double(data), 8);
-
-% Create 1ms source bitstream, 64QAM
-bitstream_duration = 1e-3;
-bits_per_symbol = 6;
-
-fs = 30.72e6;   % Sample frequency
-f = 15e3;       % 15KHz, subcarrier bandwidth
-T = 1/f;        % 60us
-
-% Repeat bitstream until its duration is 1ms
-for index=bitstream_duration/T
-    bitstream = cat(1, bitstream, bitstream);
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Constellations
 
 % Init some constants
@@ -40,6 +19,7 @@ qam_16 = [...
     b-b, b-c, c-b, c-c,...
     -b+b, -b+c, -c+b, -c+c,...
     -b-b, -b-c, -c-b, -c-c];
+qam_64_key = 0:1:63;
 qam_64 = [...
     e+e, e+d, d+e, d+d, e+f, e+g, d+f, d+g,...
     f+e, f+d, g+e, g+d, f+f, f+g, g+f, g+g,...
@@ -49,6 +29,7 @@ qam_64 = [...
     -f+e, -f+d, -g+e, -g+d, -f+f, -f+g, -g+f, -g+g,...
     -e-e, -e-d, -d-e, -d-d,-e-f, -e-g, -d-f, -d-g,...
     -f-e, -f-d, -g-e, -g-d,-f-f, -f-g, -g-f, -g-g];
+qam_64_mod = dictionary(qam_64_key ,qam_64);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
