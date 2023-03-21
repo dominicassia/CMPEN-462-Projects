cd5f554c2b1984441a7f29c789550435ec53e1aa
eliasHw <76268791+eliasHw@users.noreply.github.com>
2023-03-20 21:15:41 -0400

    Implement 64QAM; Added notes

--- Project 1/main.m
+++ Project 1/main.m
@@ -1,49 +1,146 @@
-% Data string, 63 characters
-data = 'WirelessCommunicationSystemsandSecurityDominicAssiaEliasKhamisy';
+%% Project 1
+clc;
+clear all;
+close all;
+%%    
 
-% Convert to 8-bit ascii
-bitstream = dec2bin(double(data), 8);
+% FIXME
+% Given a string e.g "WirelessCommunicationSystemsandSecurityEliasKhamisy"
+% make src_str = str of bits
+%  e.g src_str = '010101110110100101110010011001010110110001100101011100110111001101000011011011110110110101101101011101010110111001101001011000110110000101110100011010010110111101101110010100110111100101110011011101000110010101101101011100110110000101101110011001000101001101100101011000110111010101110010011010010111010001111001010001010110110001101001011000010111001101001011011010000110000101101101011010010111001101111001';
 
-% Create 1ms source bitstream, 64QAM
-bitstream_duration = 1e-3;
-bits_per_symbol = 6;
+% the string is: "WirelessCommunicationSystemsandSecurityEliasKhamisy"
+% Covert to binary we get
+src_str = '010101110110100101110010011001010110110001100101011100110111001101000011011011110110110101101101011101010110111001101001011000110110000101110100011010010110111101101110010100110111100101110011011101000110010101101101011100110110000101101110011001000101001101100101011000110111010101110010011010010111010001111001010001010110110001101001011000010111001101001011011010000110000101101101011010010111001101111001';
 
-fs = 30.72e6;   % Sample frequency
-f = 15e3;       % 15KHz, subcarrier bandwidth
-T = 1/f;        % 60us
-
-% Repeat bitstream until its duration is 1ms
-for index=bitstream_duration/T
-    bitstream = cat(1, bitstream, bitstream);
-end
-
-% Init some constants
-a = 1/sqrt(2);
-b = 1/sqrt(10);
-c = 3/sqrt(10);
-d = 1/sqrt(42);
-e = 3/sqrt(42);
-f = 5/sqrt(42);
-g = 7/sqrt(42);
-
-% Init constellations
-bpsk = [a+a, -a-a];
-qpsk = [a+a, a-a, -a+a, -a-a];
-qam_16 = [...
-    b+b, b+c, c+b, c+c,...
-    b-b, b-c, c-b, c-c,...
-    -b+b, -b+c, -c+b, -c+c,...
-    -b-b, -b-c, -c-b, -c-c];
-qam_64 = [...
-    e+e, e+d, d+e, d+d, e+f, e+g, d+f, d+g,...
-    f+e, f+d, g+e, g+d, f+f, f+g, g+f, g+g,...
-    e-e, e-d, d-e, d-d, e-f, e-g, d-f, d-g,...
-    f-e, f-d, g-e, g-d, f-f, f-g, g-f, g-g,...
-    -e+e, -e+d -d+e, -d+d, -e+f,-e+g, -d+f, -d+g,...
-    -f+e, -f+d, -g+e, -g+d, -f+f, -f+g, -g+f, -g+g,...
-    -e-e, -e-d, -d-e, -d-d,-e-f, -e-g, -d-f, -d-g,...
-    -f-e, -f-d, -g-e, -g-d,-f-f, -f-g, -g-f, -g-g];
+% 1 sub-frame worth of 64QAM symbols has the src_str repeated 422 time
+input_str = repmat(src_str,1,422); % (1) Input stream
+% split input_str into equal size strings of size 6 for 64QAM
+qam64_vec = reshape(input_str, 6, []);
+qam64_vec = transpose(qam64_vec);
+qam64_vec = cellstr(qam64_vec);
+qam64_vec = qam64_vec(1:2048*14); % trim vector to fit 1 subframe
+% Perform bits to symbol mapping for 64QAM
+qam64_dec_vec = bin2dec(qam64_vec); % convert from binary string to decimal
+qam64_dec_vec = transpose(qam64_dec_vec);
 
 
+% FIXME - map with out constellation
+qam64_sym_vec = qammod(qam64_dec_vec,64);
+
+qam64_sym_vec = qam64_sym_vec / sqrt(42); % based on table 7.1.4-1. (2) bits to symbols 64QAM
+% Serial to parallel conversion to create OFDM vectors of 2048 symbols each
+qam64_sym_sf = reshape(qam64_sym_vec, 2048, []); % organize as 2048x14 subframe. (3) Serial to Parallel
+% Compute IFFT on the subframe
+qam64_sf_ifft = ifft(qam64_sym_sf); % (5) IFFT
+% Add source input bit stream to the .mat file
+save ofdm_tx_sym.mat input_str
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FIXME - make less tedious 
+
+% Perform Cyclic Prefix Insertion and write the CP+OFDM symbols into a file
+col = qam64_sf_ifft(:,1); % pick first column of 2048 symbols: 2048x1
+col = transpose(col);
+col_last160 = col(2048-160+1:2048);
+qam64_tx_sym_1 = [col_last160, col];
+save ofdm_tx_sym.mat qam64_tx_sym_1;
+% add cyclic check to the remaining 6 symbols
+% 2nd
+col = qam64_sf_ifft(:,2);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_2 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_2;
+% 3rd
+col = qam64_sf_ifft(:,3);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_3 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_3;
+% 4th
+col = qam64_sf_ifft(:,4);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_4 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_4;
+% 5th
+col = qam64_sf_ifft(:,5);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_5 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_5;
+% 6th
+col = qam64_sf_ifft(:,6);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_6 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_6;
+% 7th
+col = qam64_sf_ifft(:,7);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_7 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_7;
+% 8th
+col = qam64_sf_ifft(:,8);
+col = transpose(col);
+col_last160 = col(2048-160+1:2048);
+qam64_tx_sym_8 = [col_last160, col];
+save ofdm_tx_sym.mat qam64_tx_sym_8;
+% add cyclic check to the remaining 6 symbols
+% 9th
+col = qam64_sf_ifft(:,9);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_9 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_9;
+% 10th
+col = qam64_sf_ifft(:,10);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_10 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_10;
+% 11th
+col = qam64_sf_ifft(:,11);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_11 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_11;
+% 12th
+col = qam64_sf_ifft(:,12);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_12 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_12;
+% 13th
+col = qam64_sf_ifft(:,13);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_13 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_13;
+% 14th
+col = qam64_sf_ifft(:,14);
+col = transpose(col);
+col_last144 = col(2048-144+1:2048);
+qam64_tx_sym_14 = [col_last144, col];
+save ofdm_tx_sym.mat qam64_tx_sym_14;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Generate time scaled plot of OFDM transmit symbols sym_1 and sym_2
+% First we create a time vector with Ts=1/fs, with length equal to the sum
+% of the two symbol lengths
+fs = 30.72e6; % 30.72MHz frequency
+dt = 1 / fs;
+T = (0 : 2048+160+2048+144-1) * dt;
+symbol = [qam64_tx_sym_1, qam64_tx_sym_2];
+% symbol = reshape(repmat(symbol(:).',10,1),1,[]);
+figure
+subplot(2,1,1)
+plot(T, abs(symbol))
+title('Amplitude')
+subplot(2,1,2)
+plot(T, angle(symbol))
+title('Phase')
---
+++ Project 1/notes.m
@@ -0,0 +1,78 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Part 1 Notes - 
+% Data string, 63 characters
+data = 'WirelessCommunicationSystemsandSecurityDominicAssiaEliasKhamisy';
+
+% Convert to 8-bit ascii
+bitstream = dec2bin(double(data), 8);
+
+% Create 1ms source bitstream, 64QAM
+bitstream_duration = 1e-3;
+bits_per_symbol = 6;
+
+fs = 30.72e6;   % Sample frequency
+f = 15e3;       % 15KHz, subcarrier bandwidth
+T = 1/f;        % 60us
+
+% Repeat bitstream until its duration is 1ms
+for index=bitstream_duration/T
+    bitstream = cat(1, bitstream, bitstream);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Constellations
+
+% Init some constants
+a = 1/sqrt(2);
+b = 1/sqrt(10);
+c = 3/sqrt(10);
+d = 1/sqrt(42);
+e = 3/sqrt(42);
+f = 5/sqrt(42);
+g = 7/sqrt(42);
+
+% Init constellations
+bpsk = [a+a, -a-a];
+qpsk = [a+a, a-a, -a+a, -a-a];
+qam_16 = [...
+    b+b, b+c, c+b, c+c,...
+    b-b, b-c, c-b, c-c,...
+    -b+b, -b+c, -c+b, -c+c,...
+    -b-b, -b-c, -c-b, -c-c];
+qam_64 = [...
+    e+e, e+d, d+e, d+d, e+f, e+g, d+f, d+g,...
+    f+e, f+d, g+e, g+d, f+f, f+g, g+f, g+g,...
+    e-e, e-d, d-e, d-d, e-f, e-g, d-f, d-g,...
+    f-e, f-d, g-e, g-d, f-f, f-g, g-f, g-g,...
+    -e+e, -e+d -d+e, -d+d, -e+f,-e+g, -d+f, -d+g,...
+    -f+e, -f+d, -g+e, -g+d, -f+f, -f+g, -g+f, -g+g,...
+    -e-e, -e-d, -d-e, -d-d,-e-f, -e-g, -d-f, -d-g,...
+    -f-e, -f-d, -g-e, -g-d,-f-f, -f-g, -g-f, -g-g];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% 2048 OFDM sub-carriers
+% WirelessCommunicationSystemsandSecurityEliasKhamisy repeated n times
+% Covert to 1msec worth of data:
+% •	51 characters, each is 8bits for total of 51*8=408bits in the string
+% •	001101000011011011110110110101101101011101010110111001101001011000110110000101110100011010010110111101101110010100110111100101110011011101000110010101101101011100110110000101101110011001000101001101100101011000110111010101110010011010010111010001111001010001010110110001101001011000010111001101001011011010000110000101101101011010010111001101111001
+% •	1msec worth of data is 1 sub-frame which is 2 slots
+% •	The data is striped over 2048 sub-carriers over a period of 14 symbols. That is, the first 2048 symbols are sent first, then the next 2048 symbols, until the 14th 2048 symbols
+% •	One sub-frame is: 2048 * 14 symbols
+% •	The string above must be repeated 2048*14*6/ 408 = 421.6 times. We round it to 422 times
+% •	Create a vector of size 422 of the above binary string
+% •	Using the Matlab reshape() function create a matrix of 7x2048 from the vector, with each element being a string of 6bits (for 64QAM)
+% •	Convert the 6bit string into decimal number using bin2dec() function
+% •	Now we have a matric of 7x2048 of the numbers we want to convert to 64QAM symbols
+% •	Use the function y = qammod(x,64) to create the 64QAM symbols. Divide by sqrt(42) to match the table in the word doc. Now you got I and Q vectors
+% •	Pass the vector through IFFT using the Matlab IFFT command
+% •	To add the Cyclic Prefix we follow this calculation:
+    % o	After IFFT we serialize the OFDM symbols. There are total of 2048 symbols coming out of IFFT
+    % o	Since each OFDM symbol is at 15KHz, when we serialize the bandwidth is 2048x15KHz = 30.72MHz
+    % o	The time slot corresponding to 30.72MHz is 1/30.72MHz = 0.032552083usec (How much time each symbol takes to send after serialized)
+    % o	Hence, for 5.2usec we get 5.2/0.032552083 = 159.744. We round up to 160
+    % o	And, 4.7usec we get 144.384. We round down to 144
+% •	To add Cyclic Prefix we do the following in each slot of 7 by 2048
+    % o	For the first 2048 symbols in a slot we add 160 symbols equal to the symbols 2047-160+1 to 2047
+    % o	For the rest of the 6 2048 symbols in a slot we add 144 symbols equal to the symbols 2047-144+1 to 2047
