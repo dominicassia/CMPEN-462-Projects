143a758e7ab59e8b17c3a36e2103723e88731fc9
Dominic Assia <50758804+dominicassia@users.noreply.github.com>
2023-04-10 21:30:10 -0400

    Created files, began tx script

---
+++ Final Project/Rx-Script.py
@@ -0,0 +1,23 @@
+# Libraries
+######################
+
+
+
+
+######################
+# Steps
+######################
+
+# Connect to LAN
+
+# Preform key exchange
+
+# Wait for data
+## Decrypt
+
+## Check HMAC
+
+## Query API
+
+## Text to output
+
---
+++ Final Project/Tx-Script.py
@@ -0,0 +1,37 @@
+# Libraries
+######################
+from hashlib import sha512      # https://docs.python.org/3/library/hashlib.html
+from Crypto.Cipher import AES
+import RPi.GPIO as GPIO
+
+######################
+# Steps
+######################
+
+# Create LAN
+
+# Preform key exchange
+
+
+key = ''
+
+# Init the speech to text
+
+# Wait for button press
+## Convert speech to text   
+
+text = ''
+
+# Compute & Append HMAC
+hashh = sha512(text)
+plaintext = text + hashh
+plaintext_bytes = bytes(plaintext, 'utf-8')
+
+iv = ''
+
+# Encrypt text
+cipher = AES.new(key, AES.MODE_CBC, iv)
+ciphetext = cipher.encrypt(plaintext_bytes)
+
+# Send over LAN
+
