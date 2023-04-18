fb72c998abe9985322235df106b7bc3ba8a71b8e
Dominic Assia <50758804+dominicassia@users.noreply.github.com>
2023-04-18 15:35:17 -0400

    Added tx modulation

--- Project 2/mp2.m
+++ Project 2/mp2.m
@@ -1,6 +1,12 @@
+%%%%%%%%%%%%%%%  Init.  %%%%%%%%%%%%%%%  
+
 % SSRG, f(x) = x^11 + x^2 + 1
 % GF(2048) = GF(2^11)
-n = 11;
+%n = 11;
+
+% SSRG, f(x) = x^9 + x^4 + 1
+% GF(512) = GF(2^9)
+n = 9;
 
 % 15kHz frequency
 f = 15e3;
@@ -9,7 +15,7 @@ f = 15e3;
 fs = 44.1e3;
 
 % Time vector
-t = 0 : 1/fs : (1/fs)*(2^n)-(1/fs);
+t = 0 : 1/fs : (1/fs)*(2^n)/2-(1/fs);
 
 % Shift register, loading vector
 shift_reg = zeros(1,2^n);
@@ -17,6 +23,20 @@ shift_reg(1) = 1;
 
 % Output sequence
 pn_sequence = zeros(1,2^n);
+
+% Modulation (QPSK)
+a = pi/4;
+qpsk_key = 0 : 1 : 3;
+qpsk = [1*a, 3*a, 5*a, 7*a];
+qpsk_mod = dictionary(qpsk_key, qpsk); % map key to value
+
+% Modulated sequence
+qpsk_pn_seq = zeros(1,(2^n)/2);
+
+% Reference carrier
+carrier = cos(2*pi*f*t);
+
+%%%%%%%%%%%%%%%  Start  %%%%%%%%%%%%%%%  
 
 % Generate
 for i = 1:2^n
@@ -27,27 +47,35 @@ for i = 1:2^n
         shift_reg(n-j+1) = shift_reg(n-j);
     end
     % Compute feedback
-    shift_reg(1) = xor(pn_sequence(i), shift_reg(2));
+    %shift_reg(1) = xor(pn_sequence(i), shift_reg(2)); % n=11
+    shift_reg(1) = xor(pn_sequence(i), shift_reg(4)); % n=9
 end
 
-% Create waveform
-carrier = cos(2*pi*f*t);
-waveform = carrier .* pn_sequence;
+% Reshape (make str pairs for qpsk)
+qpsk_vec = transpose( reshape(pn_sequence, 2, []));
+for i = 1 : (2^n)/2
+    A = num2str(qpsk_vec(i,1));
+    B = num2str(qpsk_vec(i,2));
+    qpsk_pn_seq(i) = bin2dec( strcat(A, B));
+end
 
-% Plot PN Sequence
-subplot(5,1,1);
-plot(t, pn_sequence);
+% Modulate
+phase = qpsk_mod(qpsk_pn_seq);
+
+% Create waveform
+waveform = cos(2*pi*f*t + phase);
+
 % Plot carrier
-subplot(5,1,2);
+subplot(5,1,1);
 plot(t, carrier);
 % Plot waveform
-subplot(5,1,3);
+subplot(5,1,2);
 plot(t, waveform);
 % Plot carrier spectrum
-subplot(5,1,4);
+subplot(5,1,3);
 plot(t, fft(carrier));
 % Plot waveform spectrum
-subplot(5,1,5);
+subplot(5,1,4);
 plot(t, fft(waveform));
 
 % Play waveform
--- Project 2/mp2_Rx.m
+++ Project 2/mp2_Rx.m
@@ -1,16 +1,32 @@
+%%%%%%%%%%%%%%%  Init.  %%%%%%%%%%%%%%%  
+
 % 44.1kHz sample rate
 fs = 44.1e3;
 
+% Recording duration
+duration = 2;
+
 % Time vector
-t = 0 : 1/fs : 2;
+t = 0 : 1/fs : duration;
 
 % Create recorder
 recorder = audiorecorder(fs,8,1);
 
+% Demodulation (QPSK)
+a = pi/4;
+qpsk_key = 0 : 1 : 3;
+qpsk = [a, 3*a, 5*a, 7*a];
+qpsk_mod = dictionary(qpsk_key, qpsk); % map key to value
+
+%%%%%%%%%%%%%%%  Start  %%%%%%%%%%%%%%%  
+
 % Record audio
-recordblocking(recorder, 2);
+recordblocking(recorder, duration);
 stop(recorder);
 rx_waveform = getaudiodata(recorder);
+
+% Demodulate
+
 
 % Plot waveform
 subplot(2,1,1);
@@ -18,3 +34,5 @@ plot(t, rx_waveform);
 % Plot waveform spectrum
 subplot(2,1,2);
 plot(t, fft(rx_waveform));
+
+
